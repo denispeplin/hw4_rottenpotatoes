ae97bf251e1806100c58ae79781b807bc209a9db
Denis Peplin <denis.peplin@gmail.com>
2012-10-31 20:25:58 +0400

    Add movies to the database.

---
+++ features/step_definitions/movie_steps.rb
@@ -0,0 +1,5 @@
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    Movie.create(title: movie[:title], rating: movie[:rating], release_date: movie[:release_date])
+  end
+end
