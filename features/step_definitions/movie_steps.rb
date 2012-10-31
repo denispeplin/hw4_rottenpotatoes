1c18cf3115a9d4349ee26fbdab9b6ddeab3fedb7
Denis Peplin <denis.peplin@gmail.com>
2012-10-31 20:50:52 +0400

    Implement 'director of movie'

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -3,3 +3,8 @@ Given /the following movies exist/ do |movies_table|
     Movie.create(title: movie[:title], rating: movie[:rating], release_date: movie[:release_date])
   end
 end
+
+Then /the director of "(.*)" should be "(.*)"/ do |title, director|
+  movie = Movie.find_by_title(title)
+  movie.director.should == director
+end
