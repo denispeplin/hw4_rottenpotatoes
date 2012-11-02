9d3730b2dbe274d8adba98587afe7dc6a1b7fc8c
denispeplin <denis.peplin@gmail.com>
2012-11-02 11:23:29 +0400

    Create Movie to pass test.

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -3,8 +3,10 @@ require 'spec_helper'
 describe MoviesController do
   describe 'find movies with same director' do
     it 'should call Movie#same_director method' do
-      Movie.should_receive(:same_director).with(1)
-      post :same_director, movie_id: 1
+      movie = Movie.create
+      Movie.should_receive(:same_director).with(movie.id)
+      post :same_director, movie_id: movie.id
+      Movie.destroy_all
     end
   end
 end
